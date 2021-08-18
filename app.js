7dc7575136a1e1c9bbc9f9a6982ac0148e95ef77
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-08-18 22:41:26 +0300

    backend

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const routes = require('./routes');
+const NotFoundError = require('./errors/404-NotFoundError');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+app.use(routes);
+
+app.use(requestLogger);
+
+app.use(errors());
+
+app.use(() => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use(errorLogger);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(err.statusCode).send({ message: statusCode === 500 ? 'Что-то пошло не так' : message });
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`server started, Port: ${PORT}`);
+});
